2ae3e34aea8aeba86d70f3fbf16a1e4f5d0f7d93
unknown <riskeconomics@gmail.com>
2022-09-15 16:47:58 -0400

    game_01 historias locas

---
+++ README.md
@@ -0,0 +1,10 @@
+# Notas para el juego de historias locas
+
+concatenar cadenas de caracteres.
+supongamos que queremos crear una cadena que diga:
+Aprende a programar con -----------.
+
+lenguaje = "Python"
+print("Aprende a programar con " + lenguaje)
+print("Aprende a programar con {}".format(lenguaje))
+print(f"Aprende a programar con {lenguaje}") # f-string
---
+++ game_01_historias_locas.py
@@ -0,0 +1,13 @@
+# TODO: JUEGO: historias locas
+
+#MAD LIBS (HISTORIAS LOCAS)
+
+lenguaje = input("mejor lenguaje de programación: ")
+clave_1 = input("Relacionado con la innovación tecnológica")
+clave_2 = input("Relacionado con las ciencias de la computación ")
+
+
+
+mad_lib = f"¡Programar con el mejor lenguja del momento: {lenguaje}! ya que con el lograras {clave_1}, cualquier proyecto {clave_2} que desees emprender."
+
+print(mad_lib) 
